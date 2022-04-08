89817b696ebe5363b849162c9135a2acdbe054c9
dimabeel <dima.beel.dima@gmail.com>
2022-04-08 14:25:22 +0300

    Add scale and precision for decimal values in StockHistoryRow

--- Allocator/Allocator/DAL/Context/Configuration/StockHistoryRowEntityConfiguration.cs
+++ Allocator/Allocator/DAL/Context/Configuration/StockHistoryRowEntityConfiguration.cs
@@ -8,11 +8,13 @@ public class StockHistoryRowEntityConfiguration : IEntityTypeConfiguration<Stock
 {
     public void Configure(EntityTypeBuilder<StockHistoryRow> builder)
     {
+        const int precision = 18;
+        const int scale = 2;
         builder.HasKey(e => e.StockHistoryRowId);
         builder.HasIndex(e => e.StockHistoryRowId);
-        builder.Property(e => e.Input).IsRequired();
-        builder.Property(e => e.Profit).IsRequired();
-        builder.Property(e => e.Output).IsRequired();
+        builder.Property(e => e.Input).HasPrecision(precision, scale).IsRequired();
+        builder.Property(e => e.Profit).HasPrecision(precision, scale).IsRequired();
+        builder.Property(e => e.Output).HasPrecision(precision, scale).IsRequired();
         builder.Property(e => e.Date).IsRequired();
     }
 }
